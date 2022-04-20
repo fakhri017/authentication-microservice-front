5f1b317c465637d316987fec9836cf2692354e0d
fa-user <fakhriam017@gmail.com>
2022-04-20 15:13:47 +0100

    init

---
+++ src/app/component/login/login.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import {LoginService} from '../../service/login.service';
+import {User} from '../../model/User';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  public user :User = new User() ;
+  public profileForm : FormGroup ;
+  public invalidCredentials: boolean = false ;
+  constructor(private fb: FormBuilder , private loginService : LoginService) { }
+
+
+  ngOnInit(): void {
+     this.initFormElements() ;
+  }
+
+
+  /**
+   * login with the given credentials
+   */
+  public login() : void {
+  this.loginService.login(this.user).subscribe((result : any)=>{
+    // navigate to the next page
+  } ,
+    (error)=>{
+     if(error.status == 403){
+      this.invalidCredentials = true ;
+    }
+    });
+  }
+
+
+  /**
+   * init form elments and validators
+   * @private
+   */
+  private initFormElements() : void {
+    this.profileForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+
+}
---
+++ src/app/service/login.service.ts
@@ -0,0 +1,27 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
+import {User} from '../model/User';
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+  LOGIN_URL = 'http://localhost:8081/login';
+
+
+  constructor(private http: HttpClient) {
+  }
+
+
+  public login(user : User) {
+    const headers = new HttpHeaders();
+    headers.set('content-type', 'application/x-www-form-urlencoded');
+    return this.http.post(this.LOGIN_URL + '?username='+user.username + '&password='+user.password,{headers:  headers }) ;
+
+
+  }
+
+
+}
